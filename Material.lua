654886ffb15d66899704bdb74b93f375c6c68d49
181847 <753090084@qq.com>
2017-11-08 20:29:04 +0800

    修改Material.lua和增加方法
    统一成员变量名。
    增加一个现实Material详情的方法。

--- Material.lua
+++ Material.lua
@@ -9,14 +9,25 @@ gMaterialSet = {}
 function material:ctor(name)
     self.name = name
     self.diffuseAlbedo = {1.0, 1.0, 1.0, 1.0}
-    self.FresnelR = {0.01, 0.01, 0.01}
-    self.Roughness = 0.5f;
+    self.fresnelR = {0.01, 0.01, 0.01}
+    self.roughness = 0.5;
     
-    -- 贴图是在一个贴图集合中的贴图的名称
-    self.diffuseAlbedo = nil
-    self.NormalMap = nil
+    -- the next two map is not necessary.
+    self.diffuseMap = nil
+    self.normalMap = nil
 end
 
 function material:showDetail()
     print(string.format("\"%s\"", self.name, ))
+    print(string.format("diffuseAlbedo: %f,\t%f,\t%f,\t%f", unpack(self.diffuseAlbedo)))
+    print(string.format("fresnelR: %f,\t%f,\t%f", unpack(self.fresnelR)))
+    print(string.format("roughness: %f", self.roughness)
+    
+    if self.diffuseMap then
+        print("diffuseMap: "..self.diffuseMap)
+    end
+    
+    if self.normalMap then
+        print("diffuseMap: "..self.normalMap)
+    end
 end
