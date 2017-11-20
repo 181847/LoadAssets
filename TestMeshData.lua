be0d46ce84d03562d1eece286a0f3b00f11c73a6
181847 <753090084@qq.com>
2017-11-20 12:53:21 +0800

    测试向MeshData中添加数据

--- TestMeshData.lua
+++ TestMeshData.lua
@@ -6,3 +6,11 @@ a:addPosition(1.0, 0.5, 1.0)
 
 print('After modified.')
 a:show()
+
+a:addNormal(0.2, 0.2, 0.2)
+a:addTangentU(0.3, 0.3, 0.3)
+a:addTextureCoord(0.4, 0.4)
+
+a:addVertex(1, 1, 1, 1)
+a:addIndex(1)
+a:show()
