092da50ab916ad25f1003fd05bc6ef5865ec86a9
181847 <753090084@qq.com>
2017-11-22 13:43:00 +0800

    修改测试用例

--- Test.lua
+++ Test.lua
@@ -27,33 +27,11 @@ test_m_4.diffuseAlbedo[3] = 0.222223333
 test_m_4.normalMap = test_t_1.name
 test_m_4.diffuseMap = test_t_2.name
 
-test_g_1 = Geometry.new("box", "../base/box.obj")
-test_g_2 = Geometry.new("sphere", "../base/geoSphere.obj")
+test_g_1 = Geometry.new("box", "Tank.obj")
+test_g_2 = Geometry.new("sphere", "shapeG.obj")
 
 test_ritem_1 = RenderItem.new(test_g_1.name, test_m_1.name, "opaque")
 test_ritem_2 = RenderItem.new(test_g_2.name, test_m_2.name, "water")
-
-print()
-test_t_1:showDetail()
-test_t_2:showDetail()
-print()
-test_m_1:showDetail()
-test_m_2:showDetail()
-test_m_3:showDetail()
-test_m_4:showDetail()
-print()
-test_g_1:showDetail()
-test_g_2:showDetail()
-print()
-test_ritem_1:showDetail()
-test_ritem_2:showDetail()
-print()
-RItemPart.ShowRenderLayers()
-print()
-
--- assemble by empty set
-assemble()
-
 print()
 
 -- add each instance to specific set, 
@@ -73,3 +51,22 @@ test_ritem_1:addToGlobalSet()
 test_ritem_2:addToGlobalSet()
 -- assemble again
 assemble()
+
+
+print()
+test_t_1:showDetail()
+test_t_2:showDetail()
+print()
+test_m_1:showDetail()
+test_m_2:showDetail()
+test_m_3:showDetail()
+test_m_4:showDetail()
+print()
+test_g_1:showDetail()
+test_g_2:showDetail()
+print()
+test_ritem_1:showDetail()
+test_ritem_2:showDetail()
+print()
+RItemPart.ShowRenderLayers()
+print()
